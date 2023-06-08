bf0fd528a60bf49bb571bbe72238616a1f91360f
Shalom Ogoziem <shalomzymike@gmail.com>
2023-06-08 18:27:05 +0000

    sum of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    count = len(argv) - 1
+    result = 0
+    for i in range(count):
+        result += int(argv[i + 1])
+    print(f"{result}")

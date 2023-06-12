ead52bb9c79316c2fab1d3877206032984869345
Shalom Ogoziem <shalomzymike@gmail.com>
2023-06-12 15:20:52 +0000

    find multiples of 2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     maximum = my_list[0]
-    for i in range(len(my_list)):
-        if maximum < my_list[i]:
-            maximum = my_list[i]
+    for i in my_list:
+        if maximum < i:
+            maximum = i
     return maximum

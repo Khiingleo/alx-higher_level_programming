b15539937036735938a86d33a7978630ee7a8a61
Shalom Ogoziem <shalomzymike@gmail.com>
2023-07-10 15:33:32 +0000

    base_geometry .py and .txt files done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    A class that represents base geometry
+    """
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    A class that represents base geometry
+    """
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates 'Value'
+
+        Args:
+            name (string): the name
+            value (int): the value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: ig value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

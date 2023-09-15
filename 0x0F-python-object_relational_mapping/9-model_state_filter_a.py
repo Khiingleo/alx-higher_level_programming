00c818182ca03c4bc7dac0216b26047742c214dd
Shalom Ogoziem <shalomzymike@gmail.com>
2023-09-15 12:48:29 +0000

    close session

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter 'a'
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import engine
+
+
+if __name__ 

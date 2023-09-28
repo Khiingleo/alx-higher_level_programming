efdc98555d6913d1e788fbd242de37e5eb5bd5ed
Shalom Ogoziem <shalomzymike@gmail.com>
2023-09-28 14:14:49 -0700

    0-8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,24 +3,24 @@
 defines a function that finds the peak of a list of unsorted integers
 """
 
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
     size = len(list_of_integers)
-    
+
     if size == 1:
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
-    
-    
+
     left = 0
     right = len(list_of_integers) - 1
-    
+
     mid = int(left + right) // 2
     p = list_of_integers[mid]
-    
+
     if p > list_of_integers[mid - 1] and p > list_of_integers[mid + 1]:
         return p
     elif p < list_of_integers[mid - 1]:

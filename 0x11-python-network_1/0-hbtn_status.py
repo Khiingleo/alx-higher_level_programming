3dad394d3a698027302be303d42002a0fd4b2bcb
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-02 08:13:56 +0000

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")

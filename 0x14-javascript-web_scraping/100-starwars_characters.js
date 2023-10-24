2d309e79afcf7931601dab3be7ec7d60677f667f
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-24 14:46:52 +0000

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,14 +2,19 @@
 
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
-
 const request = require('request');
+
 request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     body = JSON.parse(body);
-    console.log(body.title);
-  } else if (response) {
-    console.log('Code: ' + response.statusCode);
+    for (const i in body.characters) {
+      request(body.characters[i], function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const mybody = JSON.parse(body);
+          console.log(mybody.name);
+        }
+      });
+    }
   } else {
     console.log('Error: ', error);
   }

c8f1581f5d650eb8d40f67058a53f6a77da670c2
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-24 14:22:18 +0000

    task 6

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -8,7 +8,9 @@ request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     body = JSON.parse(body);
     console.log(body.title);
-  } else {
+  } else if (response) {
     console.log('Code: ' + response.statusCode);
+  } else {
+    console.log('Error: ', error);
   }
 });

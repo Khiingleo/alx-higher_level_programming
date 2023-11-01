9d23eb0ef0dac4251f4aff262ac78c6114a72a7a
Shalom Michael <shalomzymike@gmail.com>
2023-11-01 16:32:55 +0100

    task 100

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var headElement = document.querySelector('header');
+        headElement.style.color = '#FF0000';
+    });
+})();
